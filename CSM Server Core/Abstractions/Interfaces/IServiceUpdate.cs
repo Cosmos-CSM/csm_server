bddff2479fd72af3ea0bd580f84397885279e43b
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-12-25 10:41:56 -0800

    patched to add service interfaces

---
+++ CSM Server Core/Abstractions/Interfaces/IServiceUpdate.cs
@@ -0,0 +1,26 @@
+﻿using CSM_Database_Core.Depots.Models;
+using CSM_Database_Core.Entities.Abstractions.Interfaces;
+
+namespace CSM_Server_Core.Abstractions.Interfaces;
+
+/// <summary>
+///     Represents update base operations from a <see cref="IEntity"/> scoped server service.
+/// </summary>
+/// <typeparam name="TEntity">
+///     Type of the <see cref="IEntity"/> service scope.
+/// </typeparam>
+public interface IServiceUpdate<TEntity>
+    : IService
+    where TEntity : class, IEntity {
+
+    /// <summary>
+    ///     Updates the given <see cref="UpdateInput{TEntity}.Entity"/> based on the <paramref name="input"/> params.
+    /// </summary>
+    /// <param name="input">
+    ///     <see cref="UpdateInput{TEntity}"/> data.
+    /// </param>
+    /// <returns>
+    ///     <see cref="UpdateOutput{T}"/> data.
+    /// </returns>
+    Task<UpdateOutput<TEntity>> Update(UpdateInput<TEntity> input);
+}
---
+++ CSM Server Core/Abstractions/Interfaces/IServiceView.cs
@@ -0,0 +1,26 @@
+﻿using CSM_Database_Core.Depots.Models;
+using CSM_Database_Core.Entities.Abstractions.Interfaces;
+
+namespace CSM_Server_Core.Abstractions.Interfaces;
+
+/// <summary>
+///     Represents view base operations from a <see cref="IEntity"/> scoped server service.
+/// </summary>
+/// <typeparam name="TEntity">
+///     Type of the <see cref="IEntity"/> service scope.
+/// </typeparam>
+public interface IServiceView<TEntity>
+    : IService
+    where TEntity : class, IEntity {
+
+    /// <summary>
+    ///     Generates a <typeparamref name="TEntity"/> view,
+    /// </summary>
+    /// <param name="input">
+    ///     <see cref="QueryInput{TEntity, TParameters}"/> data.
+    /// </param>
+    /// <returns>
+    ///     <see cref="ViewOutput{TEntity}"/> data.
+    /// </returns>
+    Task<ViewOutput<TEntity>> View(QueryInput<TEntity, ViewInput<TEntity>> input);
+}
